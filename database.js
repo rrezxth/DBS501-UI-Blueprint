3baf014600f5cc98da3a2bf12092a569e038a4b4
rrezxth <rienz.edu@icloud.com>
2024-03-21 15:03:36 -0400

    Separated dbConfig and database.js -- added new function getJobsInfo()

--- database.js
+++ database.js
@@ -1,22 +1,16 @@
+// database.js
+
 require('dotenv').config();   // required when using .env
 
 const oracledb = require('oracledb');
+const dbConfig = require('./dbConfig');
 
 async function connectToOracle() {
   let connection;
 
   try {
-    connection = await oracledb.getConnection({
-      user: process.env.DB_USER,        // DB_Username
-      password: process.env.DB_PASS,    // DB_Password
-      connectionString: process.env.DB_CONNECTIONSTRING   // Syntax is [Hostname]:[Port]/[ServiceName]
-    });
-
-     console.log('Connection established to the Database.');
-
-     // Testing some table
-    const result = await connection.execute(`SELECT * FROM hr_jobs`);
-    console.log('Query result:', result);
+    connection = await oracledb.getConnection(dbConfig); // Use dbConfig for connection details
+    console.log('Connection established to the Database.');
 
   } catch (err) {
     console.error('Error connecting to the database:', err);
@@ -31,4 +25,36 @@ async function connectToOracle() {
   }
 }
 
-connectToOracle();
+// ---------------------------------------------------------
+// OTHER functions -- call procedures here
+
+
+async function getJobsInfo() {
+  let connection;
+
+  try {
+    connection = await oracledb.getConnection(dbConfig);
+
+    const result = await connection.execute(`SELECT * FROM hr_jobs`);
+    return result.rows;
+
+  } catch (err) {
+    console.error('Error retrieving job information:', err);
+    throw err;
+  } finally {
+    if (connection) {
+      try {
+        await connection.close();
+      } catch (err) {
+        console.error(err);
+      }
+    }
+  }
+}
+
+
+
+
+
+
+module.exports = { connectToOracle, getJobsInfo };
